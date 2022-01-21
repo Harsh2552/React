356d83d5b00c2ad844778321734ed7fb2512013f
harsh <harshsuryavanshi170@gmail.com>
2022-01-21 15:57:43 +0530

    hey

---
+++ training/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import First from './first.js'
+
+function App() {
+  return (
+    <div className='App'>
+      <First />
+      
+    </div>
+    
+    
+  );
+}
+
+export default App;
---
+++ training/src/first.js
@@ -0,0 +1,143 @@
+
+export default function First() {
+ return(
+    <div className="First">
+    <h1 className='ee'>Welcome</h1>
+    
+    <p className='ss'>hello</p>
+    <h3>Javascript</h3>
+    <p>This side Harsh</p>
+    <table border="2" align="center">
+      <tr>
+        <th colSpan={6}>Users</th>
+      </tr>
+      <tr className='aa'>
+      <th>NAME</th>
+      <th>PROFILE</th>
+      <th>EmpId</th>
+      <th>COMPANY</th>
+      <th>ADDRESS</th>
+      <th>SALARY</th>
+      </tr>
+      <tr className='bb'>
+      <td>Harsh</td>
+      <td>Software Engineer</td>
+      <td>101</td>
+      <td>Trangile</td>
+      <td>Meerut</td>
+      <td>6LPA</td>
+      </tr>
+      <tr className='aa'>
+      <td>Shivam</td>
+      <td>Software Engineer</td>
+      <td>102</td>
+      <td>Trangile</td>
+      <td>Meerut</td>
+      <td>6LPA</td>
+      </tr>
+      <tr className='bb'>
+      <td>Shubhi</td>
+      <td>Software Engineer</td>
+      <td>103</td>
+      <td>Trangile</td>
+      <td>Meerut</td>
+      <td>6LPA</td>
+      </tr>
+      <tr className='aa'>
+      <td>Yatendar</td>
+      <td>Software Engineer</td>
+      <td>104</td>
+      <td>Trangile</td>
+      <td>Meerut</td>
+      <td>6LPA</td>
+      </tr>
+      <tr className='bb'>
+      <td>Manish</td>
+      <td>Software Engineer</td>
+      <td>105</td>
+      <td>Trangile</td>
+      <td>Meerut</td>
+      <td>6LPA</td>
+      </tr>
+      <tr className='aa'>
+      <td>Mrinal</td>
+      <td>Software Engineer</td>
+      <td>106</td>
+      <td>Trangile</td>
+      <td>Meerut</td>
+      <td>6LPA</td>
+      </tr>
+      <tr className='bb'>
+      <td>Sakshi</td>
+      <td>Software Engineer</td>
+      <td>107</td>
+      <td>Trangile</td>
+      <td>Meerut</td>
+      <td>6LPA</td>
+      </tr>
+    </table>
+    <ul>
+    <li><a href='https://www.flipkart.com/' target="_blank" title='Flipkard' className='uu'> Flipkart</a></li>
+    <li><a href='www.google.com' className='uu'> About</a></li>
+    <li><a href='www.google.com' className='uu'> Gallery</a></li>
+    <li><a href='www.google.com' className='uu'> Contact</a></li>
+      </ul>
+      
+      <ol>
+      <li>Harsh</li>
+      <li>Harsh</li>
+      <li>Harsh</li>
+      <li>Harsh</li>
+      </ol>
+      <ol type='A'>
+      <li>Harsh</li>
+      <li>Harsh</li>
+      <li>Harsh</li>
+      <li>Harsh</li>
+      </ol>
+      <ol start={101}>
+      <li>Harsh</li>
+      <li>Harsh</li>
+      <li>Harsh</li>
+      <li>Harsh</li>
+      </ol>
+      <ol type='I'>
+      <li>Harsh</li>
+      <li>Harsh</li>
+      <li>Harsh</li>
+      <li>Harsh</li>
+      </ol>
+
+      <ul>
+      <li>Harsh</li>
+      <li>Harsh</li>
+      <li>Harsh</li>
+      <li>Harsh</li>
+      </ul>
+
+      
+      <ul type='square'>
+      <li>Harsh</li>
+      <li>Harsh</li>
+      <li>Harsh</li>
+      <li>Harsh</li>
+      </ul>
+
+      <ul type='circle'>
+      <li>Harsh</li>
+      <li>Harsh</li>
+      <li>Harsh</li>
+      <li>Harsh</li>
+      </ul>
+      <dl>
+<dt>Coffee</dt>
+<dd>Black hot drink</dd>
+<dt>Milk</dt>
+<dd>White cold drink</dd>
+</dl>
+
+    
+  </div>
+ )
+    
+}
